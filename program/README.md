f54dc60c067c14a57e701f2c4bb79a5b8bcf56d0
danielbui12 <huytung139@gmail.com>
2023-10-17 10:33:10 +0700

    update doc :bookmark_tabs:

--- program/README.md
+++ program/README.md
@@ -1,44 +1,67 @@
 # Comptoir
 
+## I. ABOUT:
 Comptoir is an open source marketplace Solana program designed for on-chain assets. It provides creators from artists to video game builders with an easy way to create their own fully decentralised marketplace.
 Think of it like Magic Eden or Solanart but open source.
 
 Comptoir can be useful for a NFT / Video game in multiple ways : 
 
-### Taking control over your fees by :
+### :white_check_mark: Taking control over your fees by :
 * Removing Middleman fees (Traditional marketplaces)
 * Collection specific fees
 * Ignoring creators fees
 
-### Customize your front end
+### :white_check_mark: Customize your front end
 
 Given that comptoir is just a Solana program you can create your own front end to plug to it.
 This allows for you to customize the way your assets are displayed adding stats, lore, user comments etc.
 
-### Use your own currency
+### :white_check_mark: Use your own currency
 
 Unlike traditional marketplaces Comptoir allows you to trade in any SPL tokens. So if your game / ecosystem has one you can set it as a currency of the marketplace
 
-### Buy Offer
+### :white_check_mark: Buy Offer
 
 We are planing to add a lot of features to Comptoir outside standard buying and selling.
 The first one is the Buy Offer. If an item is not listed in the marketplace then one can create an offer for it.
 Any seller owning that asset can then at any time accept the offer thus transferring the asset to the creator of the offer and receiving the funds.
 
-### Requirement
+## II. Requirement
 
-- solana v1.14.18
+- solana-cli v1.14.18
+> If you face with this error `libssl.so.1.1: cannot open shared object file: No such file or directory`, just read [this answer](https://stackoverflow.com/a/72633324)
 - node >=v16 <=v18
-- Run test 
-```sh
-$ solana-test-validator --bpf-program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA token_metadata_program.so --bpf-program ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL associated_token_metadata_program.so --bpf-program metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s metaplex_token_metadata_program.so --reset
 
+## III. Testing on localhost
+
+### 1. Testing with Anchor only
+I've configured `Anchor.toml` file, so feel free to start hacking, just run: 
+```sh
+$ anchor test
+```
+
+### 2. Testing with separated local validator
+
+- Because Token, Associated Token, Metaplex are not available in localhost, so we've to clone it. [Following this answer](https://solana.stackexchange.com/a/1885)
+
+```sh
+$ solana program dump -u m TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA  program_modules/token_metadata_program.so
+
+$ solana program dump -u m ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL program_modules/associated_token_metadata_program.so
+
+$ solana program dump -u m metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s program_modules/metaplex_token_metadata_program.so
+```
+
+- Run local validator
+```sh
+$ solana-test-validator --bpf-program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA program_modules/token_metadata_program.so --bpf-program ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL program_modules/associated_token_metadata_program.so --bpf-program metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s program_modules/metaplex_token_metadata_program.so --reset
+```
+
+- Run test
+```sh
 $ anchor test --skip-local-validator
 ```
 
 ## Gitbook
 
-To understand the key concepts and get started creating your own marketplace check out the gitbook [here](https://aurory.gitbook.io/comptoir/)
-
-
-https://solana.stackexchange.com/a/1885
+To thoroughly understand the key concepts and get started creating your own marketplace check out the gitbook [here](https://aurory.gitbook.io/comptoir/)
--- program/tests/sell_orders.ts
+++ program/tests/sell_orders.ts
@@ -12,7 +12,7 @@ import {
 import assert from "assert";
 import { nft_data } from "../utils/helper";
 import { mintNFT } from "../utils/utils";
-import { Comptoir, Collection, getSellOrderPDA, getCollectionPDA, getNftVaultPDA } from "../comptoirjs";
+import { Comptoir, Collection, getSellOrderPDA, getCollectionPDA, getNftVaultPDA, getComptoirPDA, getAssociatedTokenAddress } from "../comptoirjs";
 import { confirmTx } from "../utils/helper";
 
 const provider = anchor.getProvider()
@@ -20,13 +20,13 @@ anchor.setProvider(provider);
 
 describe('multi sell orders test', () => {
 	let creator: web3.Keypair;
-  let creatorTokenAccount: Account;
+	let sellerAssociatedTokenAccount: web3.PublicKey;
 	let seller: web3.Keypair;
-  let sellerTokenAccount: Account;
+  let sellerTokenAccount: web3.PublicKey;
   let comptoirMint: web3.PublicKey;
   let nftMint: web3.PublicKey;
-	let metadataPDA: web3.PublicKey;
-	let sellerNftAssociatedTokenAccount: web3.PublicKey;
+  let sellPrice: anchor.BN = new anchor.BN(1000);
+  let sellQuantity: anchor.BN = new anchor.BN(1);
 
 	let comptoir: Comptoir;
 	let collection: Collection;
@@ -55,92 +55,85 @@ describe('multi sell orders test', () => {
       6
     );
 
-    // creatorTokenAccount = await getOrCreateAssociatedTokenAccount(
-    //   provider.connection, creator, comptoirMint, creator.publicKey
-		// );
-    // console.log('creatorTokenAccount', creatorTokenAccount.address);
-
-    // sellerTokenAccount = await getOrCreateAssociatedTokenAccount(
-    //   provider.connection, seller, comptoirMint, seller.publicKey,
-		// );
-    // console.log('sellerTokenAccount', sellerTokenAccount.address);
-
-		const metadata = nft_data(creator.publicKey);
-    const { mint, tokenAccount, metadataAccount, masterEditionAccount } = await mintNFT(
-			provider.connection,
+    const metadata = nft_data(creator.publicKey);
+    const { mint } = await mintNFT(
+      provider.connection,
       creator,
       metadata,
     );
+    nftMint = mint
 
-    // metadataPDA = metadataAccount
-    // sellerNftAssociatedTokenAccount = (await getOrCreateAssociatedTokenAccount(provider.connection, creator, mint, creator.publicKey)).address;
+    sellerTokenAccount = getAssociatedTokenAddress(seller.publicKey, comptoirMint);
+    console.log('sellerTokenAccount', sellerTokenAccount.toString());
+    sellerAssociatedTokenAccount = getAssociatedTokenAddress(seller.publicKey, nftMint);
+    console.log('sellerAssociatedTokenAccount', sellerAssociatedTokenAccount.toString());
+   
+    const comptoirPDA = getComptoirPDA(creator.publicKey)
+    comptoir = new Comptoir(provider, comptoirPDA);
+  
+    console.log('Creating comptoir...')
+		await comptoir.createComptoir(seller, comptoirMint, 5, sellerTokenAccount)
+    console.log('Created comptoir')
+	
+    console.log('Creating collection...')
+    await comptoir.createCollection(seller, "aurorian", creator.publicKey, "AURY", false, 2)
+    console.log('Created collection')
 
-		// // comptoir = new Comptoir(provider)
-		// // await comptoir.createComptoir(seller, comptoirMint, 5, sellerTokenAccount.address)
-		// // await comptoir.createCollection(seller, "AURY", creator.publicKey, "AURY", false)
-
-		// // let collectionPDA = getCollectionPDA(comptoir.comptoirPDA, "AURY")
-		// // collection = new Collection(provider, collectionPDA, comptoir)
+    const collectionPDA = getCollectionPDA(comptoir.comptoirPDA, "aurorian")
+    collection = new Collection(provider, collectionPDA, comptoir)
 	});
 
-	// it('sell and buy multiple orders', async function () {
-	// 	await collection.sellAsset(
-	// 		nftMint,
-	// 		sellerNftAssociatedTokenAccount,
-	// 		sellerTokenAccount.address,
-	// 		new anchor.BN(2000),
-	// 		new anchor.BN(2),
-	// 		seller,
-	// 	)
+	it('sell and buy multiple orders', async function () {
+    console.log('Selling asset...')
+		await collection.sellAsset(
+			nftMint,
+      sellerAssociatedTokenAccount,
+      sellerTokenAccount,
+			sellPrice,
+			sellQuantity,
+			seller,
+		)
+    console.log('Created asset')
 
-	// 	await collection.sellAsset(
-	// 		nftMint,
-	// 		sellerNftAssociatedTokenAccount,
-	// 		sellerTokenAccount.address,
-	// 		new anchor.BN(2200),
-	// 		new anchor.BN(2),
-	// 		seller,
-	// 	)
+    // let sellerAfterSell = await getAccount(provider.connection, sellerNftAssociatedTokenAccount);
+    // assert.equal(Number(sellerAfterSell.amount), 1)
 
-  //   let sellerAfterSell = await getAccount(provider.connection, sellerNftAssociatedTokenAccount);
-  //   assert.equal(Number(sellerAfterSell.amount), 1)
+		// let nftVaultAddr = getNftVaultPDA(nftMint)
+		// let vaultAfterSell = await getAccount(provider.connection, nftVaultAddr)
 
-	// 	let nftVaultAddr = getNftVaultPDA(nftMint)
-	// 	let vaultAfterSell = await getAccount(provider.connection, nftVaultAddr)
+		// assert.equal(Number(vaultAfterSell.amount), 4)
 
-	// 	assert.equal(Number(vaultAfterSell.amount), 4)
+		// let buyer = anchor.web3.Keypair.generate()
+		// let fromAirdropSignature = await provider.connection.requestAirdrop(
+		// 	buyer.publicKey,
+		// 	anchor.web3.LAMPORTS_PER_SOL,
+		// );
+    // await confirmTx(provider.connection, fromAirdropSignature)
 
-	// 	let buyer = anchor.web3.Keypair.generate()
-	// 	let fromAirdropSignature = await provider.connection.requestAirdrop(
-	// 		buyer.publicKey,
-	// 		anchor.web3.LAMPORTS_PER_SOL,
-	// 	);
-  //   await confirmTx(provider.connection, fromAirdropSignature)
+    // let buyerTokenATA = await createAssociatedTokenAccount(provider.connection, buyer, comptoirMint, buyer.publicKey)
+    // await mintTo(provider.connection, buyer, comptoirMint, buyerTokenATA, seller, 8400)
 
-  //   let buyerTokenATA = await createAssociatedTokenAccount(provider.connection, buyer, comptoirMint, buyer.publicKey)
-  //   await mintTo(provider.connection, buyer, comptoirMint, buyerTokenATA, seller, 8400)
+    // let buyerNftATA = await createAssociatedTokenAccount(provider.connection, buyer, nftMint, buyer.publicKey)
 
-  //   let buyerNftATA = await createAssociatedTokenAccount(provider.connection, buyer, nftMint, buyer.publicKey)
+		// await collection.buy(
+		// 	nftMint,
+		// 	[
+		// 		getSellOrderPDA(sellerNftAssociatedTokenAccount, new anchor.BN(2000)),
+		// 		getSellOrderPDA(sellerNftAssociatedTokenAccount, new anchor.BN(2200)),
+		// 	],
+		// 	buyerNftATA,
+		// 	buyerTokenATA,
+		// 	new anchor.BN(4),
+		// 	buyer,
+		// )
 
-	// 	await collection.buy(
-	// 		nftMint,
-	// 		[
-	// 			getSellOrderPDA(sellerNftAssociatedTokenAccount, new anchor.BN(2000)),
-	// 			getSellOrderPDA(sellerNftAssociatedTokenAccount, new anchor.BN(2200)),
-	// 		],
-	// 		buyerNftATA,
-	// 		buyerTokenATA,
-	// 		new anchor.BN(4),
-	// 		buyer,
-	// 	)
+		// let buyerNftAccountAfterSell = await getAccount(provider.connection, buyerNftATA)
+		// assert.equal(Number(buyerNftAccountAfterSell.amount), 4)
 
-	// 	let buyerNftAccountAfterSell = await getAccount(provider.connection, buyerNftATA)
-	// 	assert.equal(Number(buyerNftAccountAfterSell.amount), 4)
+		// let buyerTokenAccountAfterSell = await getAccount(provider.connection, buyerTokenATA)
+		// assert.equal(Number(buyerTokenAccountAfterSell.amount), 0)
 
-	// 	let buyerTokenAccountAfterSell = await getAccount(provider.connection, buyerTokenATA)
-	// 	assert.equal(Number(buyerTokenAccountAfterSell.amount), 0)
-
-	// 	let creatorTokenAccountAfterSell = await getAccount(provider.connection, creatorTokenAccount.address)
-	// 	assert.equal(Number(creatorTokenAccountAfterSell.amount), 840)
-	// });
+		// let creatorTokenAccountAfterSell = await getAccount(provider.connection, creatorTokenAccount.address)
+		// assert.equal(Number(creatorTokenAccountAfterSell.amount), 840)
+	});
 });
