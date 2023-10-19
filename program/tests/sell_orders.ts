a96e75cf026d66c4197c2976278c4d4347f1c71f
danielbui12 <huytung139@gmail.com>
2023-10-19 13:16:10 +0700

    updated sell_order test :green_circle:

--- program/tests/sell_orders.ts
+++ program/tests/sell_orders.ts
@@ -4,8 +4,6 @@ import { web3 } from '@project-serum/anchor';
 import { 
   createMint as createMintToken,
   getOrCreateAssociatedTokenAccount,
-  createAssociatedTokenAccount,
-  Account,
   getAccount,
   mintTo,
 } from "@solana/spl-token";
@@ -20,8 +18,8 @@ anchor.setProvider(provider);
 
 describe('multi sell orders test', () => {
 	let creator: web3.Keypair;
-	let sellerAssociatedTokenAccount: web3.PublicKey;
 	let seller: web3.Keypair;
+  let sellerNftAccount: web3.PublicKey;
   let sellerTokenAccount: web3.PublicKey;
   let comptoirMint: web3.PublicKey;
   let nftMint: web3.PublicKey;
@@ -33,13 +31,15 @@ describe('multi sell orders test', () => {
 
 
 	it('Prepare tests variables', async () => {
+    /////////////// INIT CREATOR ///////////////////
 		creator = anchor.web3.Keypair.generate()
 		let fromAirdropSignature = await provider.connection.requestAirdrop(
 			creator.publicKey,
 			anchor.web3.LAMPORTS_PER_SOL,
 		);
-    await confirmTx(provider.connection, fromAirdropSignature);
+    await confirmTx(provider.connection, fromAirdropSignature);    
 
+    /////////////// INIT SELLER //////////////////
 		seller = anchor.web3.Keypair.generate()
 		fromAirdropSignature = await provider.connection.requestAirdrop(
 			seller.publicKey,
@@ -47,93 +47,110 @@ describe('multi sell orders test', () => {
 		);
     await confirmTx(provider.connection, fromAirdropSignature);
 
+    /////////////// INIT TOKEN //////////////////
     comptoirMint = await createMintToken(
       provider.connection,
-      seller,
-      seller.publicKey,
+      creator,
+      creator.publicKey,
       null,
       6
     );
+    const ata = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      seller,
+      comptoirMint,
+      seller.publicKey,
+    )
+    await mintTo(provider.connection, seller, comptoirMint, ata.address, creator, 1000)
 
-    const metadata = nft_data(creator.publicKey);
+    ///////////////// INIT NFT ////////////////
+    const metadata = nft_data(seller.publicKey);
     const { mint } = await mintNFT(
       provider.connection,
-      creator,
+      seller,
       metadata,
     );
     nftMint = mint
 
+    /////////////// INIT PDA //////////////////
     sellerTokenAccount = getAssociatedTokenAddress(seller.publicKey, comptoirMint);
     console.log('sellerTokenAccount', sellerTokenAccount.toString());
-    sellerAssociatedTokenAccount = getAssociatedTokenAddress(seller.publicKey, nftMint);
-    console.log('sellerAssociatedTokenAccount', sellerAssociatedTokenAccount.toString());
-   
-    const comptoirPDA = getComptoirPDA(creator.publicKey)
-    comptoir = new Comptoir(provider, comptoirPDA);
-  
+    sellerNftAccount = getAssociatedTokenAddress(seller.publicKey, nftMint);
+    console.log('sellerNftAccount', sellerNftAccount.toString());
+
+    ///////////// INIT COLLECTION /////////////
+    comptoir = new Comptoir(provider, getComptoirPDA(creator.publicKey));  
     console.log('Creating comptoir...')
 		await comptoir.createComptoir(seller, comptoirMint, 5, sellerTokenAccount)
     console.log('Created comptoir')
 	
     console.log('Creating collection...')
-    await comptoir.createCollection(seller, "aurorian", creator.publicKey, "AURY", false, 2)
+    await comptoir.createCollection(seller, "aurorian", seller.publicKey, "AURY", false, 2)
     console.log('Created collection')
 
     const collectionPDA = getCollectionPDA(comptoir.comptoirPDA, "aurorian")
     collection = new Collection(provider, collectionPDA, comptoir)
-	});
+  });
 
-	it('sell and buy multiple orders', async function () {
+  it('sell and buy multiple orders', async function () {
+    /////////////// SELL ASSET //////////////////
     console.log('Selling asset...')
-		await collection.sellAsset(
-			nftMint,
-      sellerAssociatedTokenAccount,
+    await collection.sellAsset(
+      nftMint,
+      sellerNftAccount,
       sellerTokenAccount,
-			sellPrice,
-			sellQuantity,
-			seller,
-		)
+      sellPrice,
+      sellQuantity,
+      seller,
+    )
     console.log('Created asset')
 
-    // let sellerAfterSell = await getAccount(provider.connection, sellerNftAssociatedTokenAccount);
-    // assert.equal(Number(sellerAfterSell.amount), 1)
+    const sellerAfterSell = await getAccount(provider.connection, sellerNftAccount);
+    assert.equal(Number(sellerAfterSell.amount), 0)
 
-		// let nftVaultAddr = getNftVaultPDA(nftMint)
-		// let vaultAfterSell = await getAccount(provider.connection, nftVaultAddr)
+    const vaultAfterSell = await getAccount(provider.connection, getNftVaultPDA(nftMint))
+		assert.equal(Number(vaultAfterSell.amount), 1)
+    
+    /////////////// INIT BUYER //////////////////
+		const buyer = anchor.web3.Keypair.generate()
+		const fromAirdropSignature = await provider.connection.requestAirdrop(
+			buyer.publicKey,
+			anchor.web3.LAMPORTS_PER_SOL,
+		);
+    await confirmTx(provider.connection, fromAirdropSignature)
 
-		// assert.equal(Number(vaultAfterSell.amount), 4)
+    const buyerTokenATA = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      buyer,
+      comptoirMint,
+      buyer.publicKey
+    )
+    await mintTo(provider.connection, buyer, comptoirMint, buyerTokenATA.address, creator, 8400)
 
-		// let buyer = anchor.web3.Keypair.generate()
-		// let fromAirdropSignature = await provider.connection.requestAirdrop(
-		// 	buyer.publicKey,
-		// 	anchor.web3.LAMPORTS_PER_SOL,
-		// );
-    // await confirmTx(provider.connection, fromAirdropSignature)
+    const buyerNftATA = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      buyer,
+      nftMint,
+      buyer.publicKey
+    );
+    const sellOrderPDA = getSellOrderPDA(sellerNftAccount, sellPrice);
+    
+    /////////////// BUY ASSET //////////////////
+    console.log('Buying asset...')
+		await collection.buy(
+			nftMint,
+			[sellOrderPDA],
+			buyerNftATA.address,
+			buyerTokenATA.address,
+      sellQuantity,
+			buyer,
+		).catch(console.log)
+    console.log('Bought asset')
 
-    // let buyerTokenATA = await createAssociatedTokenAccount(provider.connection, buyer, comptoirMint, buyer.publicKey)
-    // await mintTo(provider.connection, buyer, comptoirMint, buyerTokenATA, seller, 8400)
+		const buyerNftAccountAfterSell = await getAccount(provider.connection, buyerNftATA.address)
+		assert.equal(Number(buyerNftAccountAfterSell.amount), 1)
 
-    // let buyerNftATA = await createAssociatedTokenAccount(provider.connection, buyer, nftMint, buyer.publicKey)
-
-		// await collection.buy(
-		// 	nftMint,
-		// 	[
-		// 		getSellOrderPDA(sellerNftAssociatedTokenAccount, new anchor.BN(2000)),
-		// 		getSellOrderPDA(sellerNftAssociatedTokenAccount, new anchor.BN(2200)),
-		// 	],
-		// 	buyerNftATA,
-		// 	buyerTokenATA,
-		// 	new anchor.BN(4),
-		// 	buyer,
-		// )
-
-		// let buyerNftAccountAfterSell = await getAccount(provider.connection, buyerNftATA)
-		// assert.equal(Number(buyerNftAccountAfterSell.amount), 4)
-
-		// let buyerTokenAccountAfterSell = await getAccount(provider.connection, buyerTokenATA)
-		// assert.equal(Number(buyerTokenAccountAfterSell.amount), 0)
-
-		// let creatorTokenAccountAfterSell = await getAccount(provider.connection, creatorTokenAccount.address)
-		// assert.equal(Number(creatorTokenAccountAfterSell.amount), 840)
-	});
+		const buyerTokenAccountAfterSell = await getAccount(provider.connection, buyerTokenATA.address)
+    assert.equal(Number(buyerTokenAccountAfterSell.amount), 7400)
+  });
 });
